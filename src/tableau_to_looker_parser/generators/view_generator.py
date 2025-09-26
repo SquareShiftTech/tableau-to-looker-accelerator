b8428efb7bec28f96b64769e915b22aa71a1eb6f
Nishok-A-P <nishok@squareshift.co>
2025-09-26 19:56:25 +0530

    measure names bug fix

--- src/tableau_to_looker_parser/generators/view_generator.py
+++ src/tableau_to_looker_parser/generators/view_generator.py
@@ -566,7 +566,6 @@ TODO: Manual migration required - please convert this formula manually""",
         calc_name = self._extract_calculation_name(calc_field)
         original_formula = calculation.get("original_formula", "")
 
-
         # Create hidden dimension for row-level calculation
         dimension_field = {
             "name": f"{calc_name}_calc",  # Use calculation ID + _calc suffix
@@ -600,6 +599,8 @@ TODO: Manual migration required - please convert this formula manually""",
             ),
             "is_two_step_measure": True,  # Flag for template
             "references_dimension": f"{calc_name}_calc",  # Reference to dimension
+            "datasource_id": calc_field.get("datasource_id", ""),
+            "local_name": calc_field.get("local_name", ""),
             "default_format": calc_field.get("default_format", ""),
         }
 
